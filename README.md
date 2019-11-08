d6bc5c30297c8cfd89baacfb1e69efe36cd41b56
Achim Königs <garfonso@mobo.info>
2019-11-08 12:47:13 +0100

    initial commit
    initial commit.
    Plug DSP W-215 working
    Motion Detector DCH-S150 working

---
+++ README.md
@@ -0,0 +1,46 @@
+![Logo](admin/mydlink.png)
+# ioBroker.mydlink
+=================
+
+![Number of Installations](http://iobroker.live/badges/mydlink-installed.svg) ![Number of Installations](http://iobroker.live/badges/mydlink-stable.svg) [![NPM version](http://img.shields.io/npm/v/iobroker.mydlink.svg)](https://www.npmjs.com/package/iobroker.mydlink)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.mydlink.svg)](https://www.npmjs.com/package/iobroker.mydlink)
+[![Tests](https://travis-ci.org/arteck/ioBroker.mydlink.svg?branch=master)](https://travis-ci.org/arteck/ioBroker.mydlink)
+
+[![NPM](https://nodei.co/npm/iobroker.mydlink.png?downloads=true)](https://nodei.co/npm/iobroker.mydlink/)
+
+
+
+MyDlink Adapter for ioBroker. 
+------------------------------------------------------------------------------
+
+Currently only supports dsp-w215 smart plug.
+You need to specify IP and PIN for every device. 
+PIN is printed on the bottom of the device.
+
+## Changelog
+
+### 0.0.1
+* (Garfonso) initial release
+
+## License
+MIT License
+
+Copyright (c) 2019 Author <author@mail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,523 @@
+/**
+ *
+ *      ioBroker mydlink Adapter
+ *
+ *      (c) 2019 Garfonso <garfonso@mobo.info>
+ *
+ *      MIT License
+ *
+ */
+
+'use strict';
+/*
+ * Created with @iobroker/create-adapter v1.17.0
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+
+// Load your modules here, e.g.:
+// const fs = require("fs");
+const createSoapClient = require("./lib/soapclient.js");
+
+const readySuffix = ".ready";
+const enabledSuffix = ".enabled";
+const stateSuffix = ".state";
+const powerSuffix = ".currentPower";
+const totalPowerSuffix = ".totalPower";
+const temperatureSuffix = ".temperature";
+const lastDetectedSuffix = ".lastDetected";
+
+class DlinkSmarhome extends utils.Adapter {
+    /**
+     * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: 'mydlink',
+        });
+        this.on('ready', this.onReady.bind(this));
+        this.on('objectChange', this.onObjectChange.bind(this));
+        this.on('stateChange', this.onStateChange.bind(this));
+        // this.on('message', this.onMessage.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+
+        /**
+         * Array of devices.
+         *  Device consists of:
+         *      config: which includes IP, PIN, ... set by the user
+         *      client: soapclient for interaction with device
+         * @type {Array}
+         */
+        this.devices = [];
+    }
+
+    /**
+     * Create states for new devices, ie devices that came from config but did not have a device created, yet.
+     * @param device
+     * @returns {Promise<void>}
+     */
+    async createNewDevice(device) {
+        //also set the native part of the device:
+        await this.createDeviceAsync(device.name, {name: device.name}, device);
+        //create state object, for plug this is writable for sensor not.
+        await this.setObjectAsync(device.name + stateSuffix, {
+            type: 'state',
+            common: {
+                name: 'state',
+                type: 'boolean',
+                role: 'switch',
+                read: true,
+                write: true
+            },
+            native: {}
+        });
+        await this.setObjectAsync(device.name + enabledSuffix, {
+            type: 'state',
+            common: {
+                name: 'enabled',
+                type: 'boolean',
+                role: 'indicator',
+                read: true,
+                write: false
+            },
+            native: {}
+        });
+    }
+
+    /**
+     * Creates objects based on flags on device. IE for W215 also temperature and power measurement.
+     * @param device
+     * @returns {Promise<void>}
+     */
+    async createObjects(device) {
+        if(device.hasTemp) {
+            this.log.debug("Creating temp object for " + device.name);
+            await this.setObjectAsync(device.name + temperatureSuffix, {
+                type: 'state',
+                common: {
+                    name: 'temperature',
+                    type: 'number',
+                    role: 'value.temperature',
+                    unit: "°C",
+                    read: true,
+                    write: false
+                },
+                native: {}
+            });
+        }
+        if (device.hasPower) {
+            this.log.debug("Creating power object for " + device.name);
+            await this.setObjectAsync(device.name + powerSuffix, {
+                type: 'state',
+                common: {
+                    name: 'currentPowerConsumption',
+                    type: 'number',
+                    role: 'value.power.consumption',
+                    unit: "Wh",
+                    read: true,
+                    write: false
+                },
+                native: {}
+            });
+        }
+        if (device.hasTotalPower) {
+            this.log.debug("Creating totalPower object for " + device.name);
+            await this.setObjectAsync(device.name + totalPowerSuffix, {
+                type: 'state',
+                common: {
+                    name: 'totalPowerConsumption',
+                    type: 'number',
+                    role: 'value.power.consumption',
+                    unit: "kWh",
+                    read: true,
+                    write: false
+                },
+                native: {}
+            });
+        }
+        if (device.hasLastDetected) {
+            this.log.debug("Creating lastDetected object for " + device.name);
+            await this.setObjectAsync(device.name + lastDetectedSuffix, {
+                type: 'state',
+                common: {
+                    name: 'lastDetected',
+                    type: 'number',
+                    role: 'value.time',
+                    read: true,
+                    write: false
+                },
+                native: {}
+            });
+        }
+
+        if (!device.canSwitchOnOff) {
+            this.log.debug("Changing state to indicator for " + device.name);
+            await this.setObjectAsync(device.name + stateSuffix, {
+                type: 'state',
+                common: {
+                    name: 'state',
+                    type: 'boolean',
+                    role: 'indicator',
+                    read: true,
+                    write: false
+                },
+                native: {}
+            });
+        } else {
+            await this.subscribeStatesAsync(device.name + stateSuffix);
+        }
+
+        //have ready indicator:
+        await this.setObjectAsync(device.name + readySuffix, {
+            type: 'state',
+            common: {
+                name: "ready",
+                type: "number",
+                role: "indicator",
+                read: true,
+                write: false
+            },
+            native: {}
+        });
+    }
+
+    /**
+     * Reads settings from device and sets flags for object creation and polling.
+     * @param device
+     * @returns {Promise<*>}
+     */
+    async identifyDevice(device) {
+        //get device settings, which returns modelname and firmware version. So we know what states to create.
+        let settings = await device.client.getDeviceSettings();
+        this.log.debug(device.name + " returned following device settings: " + JSON.stringify(settings, null, 2));
+        device.model = settings.ModelName;
+
+        let soapactions = await device.client.getModuleSOAPActions(0);
+        this.log.debug("Module SOAP Actions: " + JSON.stringify(soapactions, null, 2));
+
+        switch(device.model) {
+            case "DSP-W215":
+                device.canSwitchOnOff = true;
+                device.hasTemp = true;
+                device.hasPower = true;
+                device.hasTotalPower = true;
+                device.hasLastDetected = false;
+                break;
+            case "DCH-S150":
+                device.canSwitchOnOff = false;
+                device.hasTemp = false;
+                device.hasPower = false;
+                device.hasTotalPower = false;
+                device.hasLastDetected = true;
+                break;
+            default:
+                device.canSwitchOnOff = (settings.ModelDescription.indexOf("Socket") >= 0); //if is socket, probably can switch on/off.
+                device.hasTemp = false;
+                device.hasPower = false;
+                device.hasTotalPower = false;
+                device.hasLastDetected = false;
+                break;
+        }
+        await this.createObjects(device);
+        device.identified = true;
+        return device;
+    }
+
+    /**
+     * Starts log in for device. Needs to be done before additional commands can work.
+     * @param device
+     * @returns {Promise<boolean>}
+     */
+    async loginDevice(device) {
+        try {
+            let loginResult = await device.client.login();
+            this.log.debug(device.name + " successfully logged in. " + JSON.stringify(loginResult));
+            device.loggedIn = true;
+        } catch (e) {
+            device.loggedIn = false;
+            this.log.debug("Login error: " +  JSON.stringify(e, null, 2));
+            this.log.error(device.name + " could not login. Please check credentials and if device is online/connected. Error: " + JSON.stringify(e, null, 2));
+        }
+        return device.loggedIn;
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        // Initialize your adapter here
+
+        // The adapters config (in the instance object everything under the attribute "native") is accessible via
+        // this.config:
+        //ie array devices with device objects.
+        //and polling time = interval
+        this.log.debug('polling interval: ' + this.config.interval);
+        this.log.debug('devices: ' + JSON.stringify(this.config.devices, null, 2));
+
+        //compare existing and configured devices:
+        let existingDevices = await this.getDevicesAsync();
+        this.log.debug("Got devices: " + JSON.stringify(existingDevices, null, 2));
+        for (const device of this.config.devices) {
+            this.log.debug("Processing " + device.name);
+            let id = "mydlink." + this.instance + "." + device.name;
+
+            //search for configured device in existing devices.
+            let found = false;
+            for (let i = existingDevices.length - 1; i >= 0; i -= 1) {
+                if (existingDevices[i]._id === id) {
+                    this.log.debug("Device " + device.name + " already exists. Do not create.");
+                    //remove from existing devices.
+                    existingDevices.splice(i, 1);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                this.log.debug("Device " + device.name + " new. Create device.");
+                await this.createNewDevice(device);
+            }
+
+            //transfer enabled flag to object:
+            await this.setStateAsync(device.name + enabledSuffix, { val: device.enabled, ack: true });
+
+            //do this for every enabled device, ignore not enabled devices:
+            if (device.enabled) {
+                //create the soapclient
+                let client = createSoapClient({
+                    user: "Admin",
+                    password: device.pin,
+                    url: "http://" + device.ip + "/HNAP1"
+                }); //no https, sadly.
+
+                //keep config and client for later reference.
+                let internalDevice = {
+                    config: device,
+                    client: client,
+                    id: id, //for easier state updates
+                    name: device.name, //for easier logging
+                    loggedIn: false,
+                    identified: false
+                };
+                this.devices.push(internalDevice);
+
+                let interval = this.config.interval;
+                if (interval !== 0) {
+                    this.log.debug("Start polling.");
+                    if (interval < 1000) {
+                        interval = 1000; //polling once every three seconds should be enough, right?
+                    }
+                    internalDevice.interval = setInterval(this.onInterval.bind(this, internalDevice), interval);
+                } else {
+                    this.log.debug("Polling disabled");
+                }
+
+                //login:
+                await this.loginDevice(internalDevice);
+
+                if (internalDevice.loggedIn) {
+                    try {
+                        await this.identifyDevice(internalDevice);
+                    } catch (e) {
+                        this.log.error(device.name + " could not get settings: " +  + JSON.stringify(e, null, 2));
+                    }
+                }
+            }
+        }//processing devices for the first time
+
+        for (const oldDevice of existingDevices) {
+            this.log.debug(oldDevice.native.name + " not in config anymore. Delete objects.");
+            await this.deleteDeviceAsync(oldDevice.native.name);
+        }
+    }
+
+    /**
+     * Poll a value, compare it to the value already set and if different, set value in DB.
+     * @param pollFunc - function to use for polling
+     * @param id - state Id
+     * @returns {Promise<boolean>}
+     */
+    async pollAndSetState(pollFunc, id) {
+        let value = await pollFunc();
+        if (value === "OK") {
+            value = true; //for ready
+        }
+        if (value === "ERROR") {
+            //something went wrong... maybe can not read that setting at all?
+            throw "Error during reading " + id;
+        }
+        let valueSet = await this.getStateAsync(id);
+        //this.log.debug("Got state: " + JSON.stringify(valueSet, null, 2));
+        if (!valueSet || !valueSet.ack || value !== valueSet.val) {
+            //this.log.debug("Setting state to " + value + ", because " + (!valueSet) + " || " + (!valueSet.ack) + " || " + (value !== valueSet.val));
+            await this.setStateAsync(id, value, true);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Do polling here.
+     * @param device
+     * @returns {Promise<void>}
+     */
+    async onInterval(device) {
+        //this.log.debug("Polling " + device.name);
+        try {
+            if (!device.loggedIn) {
+                let result = await this.loginDevice(device);
+                if (!result) {
+                    return;
+                }
+            }
+            if (!device.identified) {
+                await this.identifyDevice(device);
+            }
+            await this.pollAndSetState(device.client.isDeviceReady, device.id + readySuffix);
+            if (device.canSwitchOnOff) {
+                await this.pollAndSetState(device.client.state, device.id + stateSuffix);
+            }
+            if (device.hasLastDetected) {
+                let detectionHappened = await this.pollAndSetState(device.client.lastDetection, device.id + lastDetectedSuffix);
+                //this.log.debug("Detection happened: " + detectionHappened);
+                if (detectionHappened) {
+                    //always set state to true, for new detections.
+                    //this.log.debug("Setting state to true.");
+                    await this.setStateAsync(device.id + stateSuffix, detectionHappened, true);
+                } else {
+                    let currState = await this.getStateAsync(device.id + stateSuffix);
+                    //this.log.debug("State currently is: " + currState.val);
+                    if (!currState || currState.val) {
+                        this.log.debug("Setting state to false.");
+                        //reset state to false on poll after detection.
+                        await this.setStateAsync(device.id + stateSuffix, false, true);
+                    }
+                }
+            }
+            if (device.hasTemp) {
+                await this.pollAndSetState(device.client.temperature, device.id + temperatureSuffix);
+            }
+            if (device.hasPower) {
+                await this.pollAndSetState(device.client.consumption, device.id + powerSuffix);
+            }
+            if (device.hasTotalPower) {
+                await this.pollAndSetState(device.client.totalConsumption, device.id + totalPowerSuffix);
+            }
+            //this.log.debug("Polling of " + device.name + " finished.");
+        } catch (e) {
+            this.log.error("Error during polling " + device.name + ": " + JSON.stringify(e, null, 2));
+            if (e.errno === 403) {
+                device.loggedIn = false; //login next polling.
+            }
+            await this.setStateAsync(device.id + readySuffix, false, true);
+        }
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            this.log.debug("Stop polling");
+            for (const device of this.devices) {
+                if (device.interval) {
+                    clearInterval(device.interval);
+                }
+            }
+
+            this.log.info('cleaned everything up...');
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    /**
+     * Is called if a subscribed object changes
+     * @param {string} id
+     * @param {ioBroker.Object | null | undefined} obj
+     */
+    onObjectChange(id, obj) {
+        if (obj) {
+            // The object was changed
+            this.log.info("object " + id + " changed: " + JSON.stringify(obj, null, 2));
+        } else {
+            // The object was deleted
+            this.log.info(`object ${id} deleted`);
+        }
+    }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    async onStateChange(id, state) {
+        if (state) {
+            // The state was changed
+            this.log.debug(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+        } else {
+            // The state was deleted
+            this.log.debug(`state ${id} deleted`);
+            return;
+        }
+
+        //only react to commands:
+        if (state.ack === false) {
+            //find devices:
+            for (const device of this.devices) {
+                let devId = device.id + stateSuffix;
+                if (id === devId) {
+                    //found device:
+                    this.log.debug("Found device to switch.");
+                    let switchFunc = device.client.off;
+                    if (state.val) {
+                        switchFunc = device.client.on;
+                    }
+
+                    try {
+                        await switchFunc();
+                        this.log.debug("Switched " + device.name + " on.");
+                        await this.pollAndSetState(device.client.state, device.id + stateSuffix);
+                    } catch(e) {
+                        this.log.error("Error while switching device " + device.name + ": " +  + JSON.stringify(e, null, 2));
+                    }
+                    break; //can stop loop.
+                }
+            }
+        }
+    }
+
+    // /**
+    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+    //  * Using this method requires "common.message" property to be set to true in io-package.json
+    //  * @param {ioBroker.Message} obj
+    //  */
+    // onMessage(obj) {
+    // 	if (typeof obj === 'object' && obj.message) {
+    // 		if (obj.command === 'send') {
+    // 			// e.g. send email or pushover or whatever
+    // 			this.log.info('send command');
+
+    // 			// Send response in callback if required
+    // 			if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+    // 		}
+    // 	}
+    // }
+
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new DlinkSmarhome(options);
+} else {
+    // otherwise start the instance directly
+    new DlinkSmarhome();
+}
