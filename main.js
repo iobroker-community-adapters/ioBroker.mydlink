ecb7ad34007dcab20521e03033673fda9e00f98e
Achim Königs <garfonso@mobo.info>
2020-01-22 21:43:27 +0100

    updated config files.
    Signed-off-by: Achim Königs <garfonso@mobo.info>

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
 
 'use strict';
 /*
- * Created with @iobroker/create-adapter v1.17.0
+ * Created with @iobroker/create-adapter v1.20.0
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
